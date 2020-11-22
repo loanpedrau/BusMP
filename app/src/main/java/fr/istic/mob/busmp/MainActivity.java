e38a351ba381bd2d91401009f86f540632783bae
lpedrau <lpedrau@oceaneconsulting.com>
2020-11-22 15:51:54 +0100

    notification

--- app/src/main/java/fr/istic/mob/busmp/MainActivity.java
+++ app/src/main/java/fr/istic/mob/busmp/MainActivity.java
@@ -1,11 +1,9 @@
-//app
-
 package fr.istic.mob.busmp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
--- app/src/main/java/fr/istic/mob/busmp/StarService.java
+++ app/src/main/java/fr/istic/mob/busmp/StarService.java
@@ -1,18 +1,27 @@
 package fr.istic.mob.busmp;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.IBinder;
 import android.os.Looper;
 import android.os.Message;
 import android.os.Process;
-
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class StarService extends Service {
 
@@ -26,6 +35,7 @@ public class StarService extends Service {
 
     @Override
     public void onCreate() {
+        createNotificationChannel();
         HandlerThread thread = new HandlerThread("ServiceStartArguments",Process.THREAD_PRIORITY_BACKGROUND);
         thread.start();
         mServiceLooper = thread.getLooper();
@@ -45,6 +55,33 @@ public class StarService extends Service {
         super.onDestroy();
     }
 
+    private void requestNewFile() throws IOException {
+        URL url;
+        HttpURLConnection urlConnection = null;
+        StringBuilder result = new StringBuilder();
+
+        try {
+            url = new URL("https://data.explore.star.fr/api/records/1.0/search/?dataset=tco-busmetro-horaires-gtfs-versions-td&q=");
+
+            urlConnection = (HttpURLConnection) url.openConnection();
+
+            InputStream in = urlConnection.getInputStream();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                result.append(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+        }
+        System.out.println(result.toString());
+    }
 
     private final class ServiceHandler extends Handler {
 
@@ -58,23 +95,43 @@ public class StarService extends Service {
 
         @Override
         public void handleMessage(Message msg){
+            try {
+                requestNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             // Create an explicit intent for an Activity in your app
             //Intent intent = new Intent(this, AlertDetails.class);
             //intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             //PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
-            System.out.println("passage");
-            NotificationCompat.Builder builder = new NotificationCompat.Builder(this.context, CHANNEL_ID)
+
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(getBaseContext(), CHANNEL_ID)
                     .setSmallIcon(R.drawable.ic_launcher_background)
                     .setContentTitle("title")
                     .setContentText("content")
-                    .setPriority(NotificationCompat.PRIORITY_HIGH);
-            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this.context);
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getBaseContext());
 
             // notificationId is a unique int for each notification that you must define
-            notificationManager.notify(1, builder.build());
-            stopSelf(msg.arg1);
+            notificationManager.notify(0, builder.build());
         }
     }
 
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "channel_name";
+            String description = "description";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(ServiceHandler.CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
 
 }

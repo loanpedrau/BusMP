68c42f714fccab9d5553495f9450b3a81eadaf6a
lpedrau <lpedrau@oceaneconsulting.com>
2020-11-26 12:33:35 +0100

    fix error rotation progress bar

--- app/src/main/java/fr/istic/mob/busmp/MainActivity.java
+++ app/src/main/java/fr/istic/mob/busmp/MainActivity.java
@@ -1,13 +1,11 @@
 package fr.istic.mob.busmp;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.view.View;
 import android.widget.ProgressBar;
 
@@ -15,7 +13,6 @@ public class MainActivity extends AppCompatActivity {
 
     private MyBroadRequestReceiver receiver;
     private ProgressBar bar;
-    private boolean progress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,12 +26,6 @@ public class MainActivity extends AppCompatActivity {
             registerReceiver( receiver, filter);
             Intent intent = new Intent(this, StarService.class);
             startService(intent);
-        }else if(savedInstanceState != null && savedInstanceState.getBoolean("PROGRESS")){
-            bar = (ProgressBar) findViewById(R.id.progressBar);
-            bar.setVisibility(View.VISIBLE);
-        }else if(savedInstanceState != null && !savedInstanceState.getBoolean("PROGRESS")){
-            bar = (ProgressBar) findViewById(R.id.progressBar);
-            bar.setVisibility(View.GONE);
         }
     }
 
@@ -43,19 +34,11 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
     }
 
-    @Override
-    public void onSaveInstanceState(Bundle outState){
-        outState.putBoolean("PROGRESS", progress);
-        super.onSaveInstanceState(outState);
-    }
-
     private class MyBroadRequestReceiver extends BroadcastReceiver {
 
         @Override
         public void onReceive(Context context, Intent intent) {
             System.out.println("update");
-            bar = (ProgressBar) findViewById(R.id.progressBar);
-            progress = true;
             int max_progress = intent.getIntExtra("max_progress",5);
             int value = intent.getIntExtra("value",1);
             System.out.println("max :"+max_progress+" value :"+value);
@@ -63,7 +46,6 @@ public class MainActivity extends AppCompatActivity {
             bar.setProgress(value);
             if(value == max_progress){
                 bar.setVisibility(View.GONE);
-                progress = false;
             }
         }
     }

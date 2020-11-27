ec9183ecb76877ec749c72ec654b7635e8dd285a
hafsa <hmallouky@gmail.com>
2020-11-27 16:14:03 +0100

    Dao interfaces Trip & StopTime

---
+++ app/src/main/java/fr/istic/mob/busmp/StopTimeDao.java
@@ -0,0 +1,27 @@
+package fr.istic.mob.busmp;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface StopTimeDao {
+
+    @Query("SELECT * FROM Stop_time WHERE trip_id = :trip_id AND stop_id = :stop_id ")
+    LiveData<List<Stop_time>> getStopTime(int trip_id, int stop_id );
+
+    @Insert
+    void insertStopTime(Stop_time StopTime);
+
+    @Update
+    void updateStopTime(Stop_time StopTime);
+
+    @Query("DELETE FROM Stop_time WHERE trip_id = :trip_id AND stop_id = :stop_id ")
+    int deleteItem(int trip_id, int stop_id  );
+
+}
+
---
+++ app/src/main/java/fr/istic/mob/busmp/TripDao.java
@@ -0,0 +1,23 @@
+package fr.istic.mob.busmp;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+import java.util.List;
+@Dao
+public interface TripDao {
+
+    @Query("SELECT * FROM Trip WHERE trip_id = :trip_id ")
+    LiveData<List<Stop_time>> getTrip(int trip_id);
+
+    @Insert
+    void insertTrip(Trip trip);
+
+    @Update
+    void updateTrip(Trip trip);
+
+    @Query("DELETE FROM Trip WHERE trip_id = :trip_id ")
+    int deleteTrip(int trip_id, int stop_id  );
+}
